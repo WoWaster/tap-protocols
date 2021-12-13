b965de8a2e372acc362c5d11a8664b28f3f97152
WoWaster <wowasterdev@gmail.com>
2021-12-13 16:55:22 +0300

    Add IP and UDP checksum

--- utils.c
+++ utils.c
@@ -12,3 +12,27 @@ int is_ip_in_subnet(struct in_addr ip)
 	}
 	return index;
 }
+
+// taken from https://android.googlesource.com/platform/system/core/+/master/libnetutils/packet.c#62
+uint32_t checksum(void *buffer, unsigned int count, uint32_t startsum)
+{
+	uint16_t *up = (uint16_t *)buffer;
+	uint32_t sum = startsum;
+	uint32_t upper16;
+	while (count > 1) {
+		sum += *up++;
+		count -= 2;
+	}
+	if (count > 0)
+		sum += (uint16_t) * (uint8_t *)up;
+
+	while ((upper16 = (sum >> 16)) != 0)
+		sum = (sum & 0xffff) + upper16;
+
+	return sum;
+}
+
+uint32_t finish_sum(uint32_t sum)
+{
+	return ~sum & 0xffff;
+}
--- utils.h
+++ utils.h
@@ -1,7 +1,11 @@
 #ifndef TAP_PROTOCOLS_UTILS_H
 #define TAP_PROTOCOLS_UTILS_H
 #include <netinet/in.h>
+#include <netinet/ip.h>
 
 int is_ip_in_subnet(struct in_addr);
+
+uint32_t checksum(void *buffer, unsigned int count, uint32_t startsum);
+uint32_t finish_sum(uint32_t sum);
 
 #endif //TAP_PROTOCOLS_UTILS_H
