92f8a30770de1496ea5764f0d44040be62d726d4
WoWaster <wowasterdev@gmail.com>
2021-12-13 16:47:28 +0300

    Start implementing IP parser

--- parser.c
+++ parser.c
@@ -1,8 +1,22 @@
 #include <net/ethernet.h>
 #include <string.h>
 #include <netinet/in.h>
+#include <netinet/ip.h>
 #include "parser.h"
 #include "protocols/arp/arp.h"
+#include "globals.h"
+
+void parse_IP(char *frame, int fd, struct ether_header *eth_header)
+{
+	struct ip ip_header;
+	memcpy(&ip_header, frame + ETHER_HEADER_OFFSET, sizeof(ip_header));
+	if (ip_header.ip_p == IPPROTO_UDP) {
+		//TODO: implement UDP
+	}
+	if (ip_header.ip_p == IPPROTO_ICMP) {
+		// TODO: implement ICMP
+	}
+}
 
 void parse_frame(char *frame, int fd)
 {
@@ -10,7 +24,6 @@ void parse_frame(char *frame, int fd)
 	memcpy(&ether_header, frame, sizeof(ether_header));
 	if (ether_header.ether_type == htons(ETHERTYPE_ARP))
 		parse_ARP(frame, fd);
-	if (ether_header.ether_type == htons(ETHERTYPE_IP)) {
-		//TODO: implement IP
-	}
+	if (ether_header.ether_type == htons(ETHERTYPE_IP))
+		parse_IP(frame, fd, &ether_header);
 }
