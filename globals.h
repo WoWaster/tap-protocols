a902fc76feb0c15dc1cdab6680bc58578e79eb9f
WoWaster <wowasterdev@gmail.com>
2021-12-13 16:29:28 +0300

    Add useful macros

--- globals.h
+++ globals.h
@@ -4,6 +4,13 @@
 #include <netinet/in.h>
 #include <stdint.h>
 
+#define ETHER_HEADER_OFFSET sizeof(struct ether_header)
+#define IP_HEADER_OFFSET sizeof(struct ether_header) + sizeof(struct ip)
+#define UDP_HEADER_OFFSET \
+	sizeof(struct ether_header) + sizeof(struct ip) + sizeof(struct udphdr)
+
+#define MAC_SIZE (sizeof(uint8_t) * ETH_ALEN)
+
 #define SUBNET_SIZE 3
 extern const struct in_addr IPS[SUBNET_SIZE];
 extern const uint8_t MACS[SUBNET_SIZE][ETH_ALEN];
